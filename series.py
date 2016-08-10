32aa07b3d9d7dedaf7f4a24e405b3b32ff179ea3
Justin Lange <well1912@gmail.com>
2016-08-09 20:38:49 -0700

    Refactor fibonacci so it's iterative

--- series.py
+++ series.py
@@ -1,7 +1,5 @@
 def fibonacci(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
+    a, b = 0, 1
+    for i in range(n):
+        a, b = a+b, a
+    return a
