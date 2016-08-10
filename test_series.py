d5d7e72b3f6d0bebe990b71c56cde2606bc5b7f3
Justin Lange <well1912@gmail.com>
2016-08-09 20:39:22 -0700

    Implement tests for Lucas series

--- test_series.py
+++ test_series.py
@@ -8,6 +8,14 @@ FIBS_TABLE = [
     (30, 832040)
 ]
 
+LUCAS_TABLE = [
+    (0, 2),
+    (1, 1),
+    (2, 3),
+    (3, 4),
+    (7, 29),
+]
+
 
 @pytest.mark.parametrize('n, result', FIBS_TABLE)
 def test_fibonacci(n, result):
@@ -15,3 +23,11 @@ def test_fibonacci(n, result):
     Fibonacci sequence."""
     from series import fibonacci
     assert fibonacci(n) == result
+
+
+@pytest.mark.parametrize('n, result', LUCAS_TABLE)
+def test_lucas(n, result):
+    """Test function `lucas' outputs correct values for the Lucas
+    sequence."""
+    from series import lucas
+    assert lucas(n) == result
